5e7d4ce61345dfa533b57e7647954667bd8186ce
Carlos San Martin <carlosanmartin@hotmail.com>
2023-11-15 00:26:55 -0300

    delete component experienceList

--- src/components/molecules/ExperienceList/index.ts
+++ src/components/molecules/ExperienceList/index.ts
@@ -1,4 +1,3 @@
 export { default as ExperienceEasy } from './ExperienceEasy';
 export * from './ExperienceList.style';
-export { default as ExperienceList } from './ExperienceList';
 export { default as ExperienceTcs } from './ExperienceTcs';
