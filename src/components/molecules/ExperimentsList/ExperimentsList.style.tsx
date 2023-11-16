96c4efc7467c5ff4e2982c2fab87fdae237276a3
Carlos San Martin <carlosanmartin@hotmail.com>
2023-11-16 15:00:10 -0300

    add info in experiment option

--- src/components/molecules/ExperimentsList/ExperimentsList.style.tsx
+++ src/components/molecules/ExperimentsList/ExperimentsList.style.tsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
-export const ContainerExperiments = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 420px;
+export const ImageExperiment = styled.img`
+  margin: 0 auto;
+  display: table;
+  border: 2px solid white;
+  text-align: center;
+  max-width: 300px;
   @media only screen and (max-width: 1024px) {
-    margin-left: 0;
-    height: 300px;
+    max-width: 250px;
   }
 `;
 
@@ -19,22 +19,107 @@ export const ContainerTextExperiments = styled.div`
 `;
 
 export const TitleExperiments = styled.div`
-  font-size: 35px;
-  line-height: 35px;
+  font-size: 26px;
+  line-height: 26px;
   font-family: ${({ theme }) => theme.fonts.Roboto};
   color: ${({ theme }) => theme.colors.title};
-  font-weight: 600;
+  font-weight: 500;
   text-align: center;
-
+  padding-bottom: 5px;
+  margin: 15px auto 5px auto;
+  max-width: 380px;
 
   @media only screen and (max-width: 1024px) {
-    font-size: 30px;
-    line-height: 30px;
+    font-size: 20px;
+    line-height: 20px;
     text-align: center;
-    margin-top: 15px;
   }
   @media only screen and (max-width: 650px) {
     font-size: 22px;
     line-height: 22px;
+  }
+`;
+
+export const TextExperiments = styled.div`
+  font-size: 18px;
+  line-height: 20px;
+  font-family: ${({ theme }) => theme.fonts.Roboto};
+  color: ${({ theme }) => theme.colors.title};
+  font-weight: 300;
+  text-align: left;
+  padding-bottom: 5px;
+  margin: 0px auto;
+  max-width: 320px;
+
+  @media only screen and (max-width: 1024px) {
+    font-size: 16px;
+    line-height: 16px;
+    text-align: center;
+    margin-top: 15px;
+  }
+  @media only screen and (max-width: 650px) {
+    font-size: 16px;
+    line-height: 16px;
+  }
+`;
+
+export const ContainerLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  max-width: 320px;
+  margin: 20px auto;
+  @media only screen and (max-width: 1024px) {
+  }
+  @media only screen and (max-width: 650px) {
+  }
+`;
+
+export const LinkExperiment = styled.div`
+  display: flex;
+  align-content: center;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
+  svg {
+    font-size: 23px;
+    fill: ${({ theme }) => theme.colors.title};
+  }
+
+  @media only screen and (max-width: 1024px) {
+  }
+  @media only screen and (max-width: 650px) {
+  }
+`;
+
+export const TextLinkExperiment = styled.div`
+  font-size: 13px;
+  font-family: ${({ theme }) => theme.fonts.Roboto};
+  color: ${({ theme }) => theme.colors.title};
+  padding: 6px 5px;
+
+  @media only screen and (max-width: 1024px) {
+  }
+  @media only screen and (max-width: 650px) {
+  }
+`;
+
+export const ContainerCard = styled.div`
+  border-bottom: 4px solid ${({ theme }) => theme.colors.title};
+  background: ${({ theme }) => theme.colors.bgCard};
+  padding-top: 20px;
+  border-radius: 5px;
+  margin-bottom: 30px;
+
+  @media only screen and (max-width: 1024px) {
+    max-width: 300px;
+    display: table;
+    margin: 0 auto;
+    padding: 20px;
+    margin-bottom: 60px;
+  }
+  @media only screen and (max-width: 650px) {
   }
 `;
--- src/components/molecules/ExperimentsList/ExperimentsList.tsx
+++ src/components/molecules/ExperimentsList/ExperimentsList.tsx
@@ -1,24 +1,119 @@
-import { CardGlass } from "@/components/atoms";
 import {
-  ContainerExperiments,
-  ContainerTextExperiments,
-  TitleExperiments
+  TextExperiments,
+  ContainerLinks,
+  ImageExperiment,
+  TitleExperiments,
+  LinkExperiment,
+  TextLinkExperiment,
+  ContainerCard,
 } from "./ExperimentsList.style";
 import { useTranslation } from "react-i18next";
+import { Row, Col } from "react-grid-system";
+import Memo from "@/assets/experiments/memo.jpg";
+import Cv from "@/assets/experiments/mycv.jpg";
+import Starwars from "@/assets/experiments/starwars.jpg";
+import { FaGithub, FaTools } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
 
 const ExperimentsList = () => {
-
   const { t } = useTranslation();
+
+  const onGetCvGit = () => {
+    window.open("https://github.com/carlos1014/my-cv", "_blank");
+  };
+
+  const onGetMemoGit = () => {
+    window.open("https://github.com/carlos1014/memory-game-react", "_blank");
+  };
+
+  const onGetMemoWeb = () => {
+    window.open("https://memorymorty.netlify.app", "_blank");
+  };
+  
   return (
     <>
-      <CardGlass>
-        <ContainerExperiments>
-          <ContainerTextExperiments>
-            <TitleExperiments>{t("experiments.coming")}</TitleExperiments>
-          </ContainerTextExperiments>
-        </ContainerExperiments>
-      </CardGlass>
-
+      <Row>
+        <Col
+          sm={12}
+          lg={6}
+          xxl={6}
+          style={{
+            marginTop: "20px",
+          }}
+        >
+          <ContainerCard>
+            <ImageExperiment src={Cv} alt="Cv" />
+            <TitleExperiments>{t("experiments.mycv.title")}</TitleExperiments>
+            <TextExperiments>{t("experiments.mycv.text")}</TextExperiments>
+            <ContainerLinks>
+              <LinkExperiment>
+                <FaLink />
+                <TextLinkExperiment>
+                  {t("experiments.common.online")}
+                </TextLinkExperiment>
+              </LinkExperiment>
+              <LinkExperiment onClick={onGetCvGit}>
+                <FaGithub />
+                <TextLinkExperiment>
+                  {t("experiments.common.git")}
+                </TextLinkExperiment>
+              </LinkExperiment>
+            </ContainerLinks>
+          </ContainerCard>
+        </Col>
+        <Col
+          sm={12}
+          lg={6}
+          xxl={6}
+          style={{
+            marginTop: "20px",
+          }}
+        >
+          <ContainerCard>
+            <ImageExperiment src={Memo} alt="Memo" />
+            <TitleExperiments>{t("experiments.memo.title")}</TitleExperiments>
+            <TextExperiments>{t("experiments.memo.text")}</TextExperiments>
+            <ContainerLinks>
+              <LinkExperiment onClick={onGetMemoWeb}>
+                <FaLink />
+                <TextLinkExperiment>
+                  {t("experiments.common.online")}
+                </TextLinkExperiment>
+              </LinkExperiment>
+              <LinkExperiment onClick={onGetMemoGit}>
+                <FaGithub />
+                <TextLinkExperiment>
+                  {t("experiments.common.git")}
+                </TextLinkExperiment>
+              </LinkExperiment>
+            </ContainerLinks>
+          </ContainerCard>
+        </Col>
+        <Col
+          sm={12}
+          lg={6}
+          xxl={6}
+          style={{
+            marginTop: "20px",
+          }}
+        >
+          <ContainerCard>
+            <ImageExperiment src={Starwars} alt="Memo" />
+            <TitleExperiments>
+              {t("experiments.starwars.title")}
+            </TitleExperiments>
+            <TextExperiments>{t("experiments.starwars.text")}</TextExperiments>
+            <ContainerLinks>
+              <LinkExperiment>
+                <FaTools />
+                <TextLinkExperiment>
+                  {t("experiments.common.under")}
+                </TextLinkExperiment>
+              </LinkExperiment>
+            </ContainerLinks>
+          </ContainerCard>
+        </Col>
+      </Row>
     </>
   );
 };
